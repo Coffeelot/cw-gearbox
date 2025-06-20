07886612bed96f0bffcedff68c90acf666c4a964
Tom <getrekttom@gmail.com>
2025-06-20 23:14:40 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -6,6 +6,7 @@ Config.NotifyManual = true -- if true then a notification will show if manual
 Config.ManualNotificationText = 'This vehicle is a manual' -- text that shows in notification
 Config.UseServerSideStateSet = true -- If true then set statebag via server. This should fix audio sync issues. Toggle to off if you want this script to just use client side
 Config.GearCheckSleep = 1000 -- Sleep, in ms, for threads verifying gear
+Config.AutoIdentifyRhd = true -- Use calculation to detect RHD vehicles
 
 Config.CwTuning = true -- Set to true if you use CW tuning and want to use that scripts gearbox swaps
 Config.UseOtherCheck = false -- set to true and add your code to client.lua. Search for "ADD YOUR CHECK HERE" in the file
